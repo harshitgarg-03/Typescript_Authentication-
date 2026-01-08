8905b49eaedfc2fc1084ef37c3e2c15f57190dac
Harshit Garg <gargh2120@gmail.com>
2026-01-08 21:35:30 +0530

    error fix

--- src/Controllers/Auth.Controller.ts
+++ src/Controllers/Auth.Controller.ts
@@ -35,6 +35,8 @@ const Generate_Access_Refresh_Token = async (
 
 
 
+
+
 export const RegisterUser = AsyncHandler(
     async (req: Request, res: Response) => {
         const { name, email, password } = req.validation;
